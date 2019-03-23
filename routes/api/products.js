a80b257a18b0bf0c526ac9187a4e27a4079d4a02
mariamwalid <mariamwalid2022@gmail.com>
2019-03-23 13:50:50 +0200

    clubs/events/products

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,77 +1,89 @@
-const express = require('express')
-const router = express.Router()
-const mongoose = require('mongoose')
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
 
-const Product = require('../../models/Product')
-
+const Product = require("../../models/Product");
+const User = require("../../models/User");
 
 // get all products
-router.get('/', async (req, res) => {
-  try{
-    const products = await Product.find()
-    res.json({ data: products })
-  }
-  catch (error) {
-    return res.sendStatus(400).json(error);
-  }
-  })
+router.get("/", async (req, res) => {
+  const products = await Product.find();
+  res.json({ data: products });
+});
 
-// get certain product by name regex, case insenstive
-router.get('/:name', async (req, res) => {
-  try{
-    const name = req.params.name
-    const product = await Product.find({ "name": { "$regex": name, "$options": "i" }  })
-    res.json({ data: product })
-  }
-  catch (error) {
-    return res.sendStatus(400).json(error);
-  }
-  })
+// get certain product by name
+router.get("/:name", async (req, res) => {
+  const name = req.params.name;
+  const product = await Product.find({ name: name });
+  res.json({ data: product });
+});
 
 // create a new product
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
-    const newProduct = await Product.create(req.body)
-    res.json({ msg: 'Product was created successfully', data: newProduct })
-  }
-  catch (error) {
+    if (!req.session.user_id)
+      return res.status(403).send({ error: "You are not logged in" });
+
+    const userOne = await User.findById(req.session.user_id);
+
+    if (
+      !(userOne.awg_admin === "mun") &&
+      !(userOne.mun_role === "secretary_office")
+    )
+      return res.status(403).send({ error: "Only admins can create products" });
+
+    const newProduct = await Product.create(req.body);
+    res.json({ msg: "Product was created successfully", data: newProduct });
+  } catch (error) {
     return res.sendStatus(400).json(error);
   }
-})
-
-
+});
 
 // update the whole product by id
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
   try {
-    const id = req.params.id
-    const product = await Product.find({ id })
-    if (!product) return res.status(404).json({ error: 'Product does not exist' })
-    const updateProduct = await Product.updateOne(req.body)
-    res.json({ msg: 'Product updated successfully' })
-  }
-  catch (error) {
+    if (!req.session.user_id)
+      return res.status(403).send({ error: "You are not logged in" });
+
+    const userOne = await User.findById(req.session.user_id);
+
+    if (
+      !(userOne.awg_admin === "mun") &&
+      !(userOne.mun_role === "secretary_office")
+    )
+      return res.status(403).send({ error: "Only admins can update products" });
+
+    const id = req.params.id;
+    const product = await Product.find({ id });
+    if (!product)
+      return res.status(404).send({ error: "Product does not exist" });
+    const updateProduct = await Product.updateOne(req.body);
+    res.json({ msg: "Product updated successfully" });
+  } catch (error) {
     return res.sendStatus(400).json(error);
   }
-})
+});
 
-
-// delete the whole product by id 
-router.delete('/:id', async (req, res) => {
+// delete the whole product by id
+router.delete("/:id", async (req, res) => {
   try {
-    const id = req.params.id
-    const deletedProduct = await Product.findByIdAndRemove(id)
-    res.json({ msg: 'Product was deleted successfully', data: deletedProduct })
-  }
-  catch (error) {
+    if (!req.session.user_id)
+      return res.status(403).send({ error: "You are not logged in" });
+
+    const userOne = await User.findById(req.session.user_id);
+
+    if (
+      !(userOne.awg_admin === "mun") &&
+      !(userOne.mun_role === "secretary_office")
+    )
+      return res.status(403).send({ error: "Only admins can delete products" });
+
+    const id = req.params.id;
+    const deletedProduct = await Product.findByIdAndRemove(id);
+    res.json({ msg: "Product was deleted successfully", data: deletedProduct });
+  } catch (error) {
     return res.sendStatus(400).json(error);
   }
-})
+});
 
 module.exports = router;
-
-
-
-
-
-
