83e85cda42983c90a3331c39ad007db1b772d2b2
Omar Hussein <omarhatem101@gmail.com>
2019-03-31 18:25:18 +0200

    Added events tests and updated deprecated Update()

--- routes/api/clubs.js
+++ routes/api/clubs.js
@@ -1,93 +1,88 @@
-const express = require("express");
-const router = express.Router();
-const passport = require("passport");
-const Club = require("../../models/Club");
-const validator = require("../../validations/clubValidations");
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const passport = require('passport')
+const Club = require('../../models/Club')
+const validator = require('../../validations/clubValidations')
+const User = require('../../models/User').model
 
-//get all clubs
-router.get("/", async (req, res) => {
-  const clubs = await Club.find();
-  res.json({ data: clubs });
+router.get('/', async (req, res) => {
+
+    const clubs = await Club.find();
+    res.json({ data: clubs });
 });
 
-//get clubs by id
-router.get("/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const club = await Club.find({ _id: id });
-    if (!club) return res.json({ message: "Club does not exist" });
+router.get('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const club = await Club.find({_id:id});
+        if (!club) return res.json({ message: 'Club does not exist' })
 
-    res.json({ msg: "Club data", data: club });
-  } catch (error) {
-    // We will be handling the error later
-    console.log(error);
-  }
+        res.json({ msg: 'Club data', data: club })
+    }
+    catch (error) {
+        // We will be handling the error later
+        console.log(error)
+    }
 });
 
-//admins create new club
-router.post(
-  "/addclub",
-  passport.authenticate("jwt", { session: false }),
-  async (req, res) => {
+
+router.post('/addclub',  passport.authenticate('jwt', {session: false}),async (req, res) => {
     try {
-      const userOne = req.user;
+       // if (!req.session.user_id) return res.json({ message: "You are not logged in" })
 
-      if (!userOne.is_admin)
-        return res.json({ message: "Only admins can add clubs" });
-      const isValidated = validator.createValidation(req.body);
-      if (isValidated.error)
-        return res.json({ msg: "validation are not satisfied" });
-      const newClub = await Club.create(req.body);
-      res.json({ msg: "Club was created successfully", data: newClub });
-    } catch (error) {
-      // We will be handling the error later
-      console.log(error);
+    const userOne = req.user
+
+    if (!userOne.is_admin) return res.json({ message: 'Only admins can add clubs' })
+        const isValidated = validator.createValidation(req.body)
+        if (isValidated.error) return   res.json({ msg: "validation are not satisfied"});
+        const newClub = await Club.create(req.body)
+        res.json({ msg: 'Club was created successfully', data:newClub })
     }
-  }
-);
+    catch (error) {
+        // We will be handling the error later
+        console.log(error)
+    }
+})
 
-//admins update club by id
-router.put(
-  "/:id",
-  passport.authenticate("jwt", { session: false }),
-  async (req, res) => {
+
+router.put('/:id',  passport.authenticate('jwt', {session: false}), async (req, res) => {
     try {
-      const userOne = req.user;
-      if (!userOne.is_admin)
-        return res.json({ msg: "Only admins can update clubs" });
-      const id = req.params.id;
-      const club = await Club.find({ _id: id });
-      if (!club) return res.json({ message: "Club does not exist" });
-      const isValidated = validator.updateValidation(req.body);
-      if (isValidated.error)
-        return res.json({ message: "validations are not satisfied" });
-      const updatedClub = await Club.updateOne(req.body);
-      res.json({ msg: "Club updated successfully", data: updatedClub });
-    } catch (error) {
-      // We will be handling the error later
-      console.log(error);
-    }
-  }
-);
+       // if (!req.session.user_id)  return  res.json({ msg: "not logged in"});
 
-//admins delete club by id
-router.delete(
-  "/:id",
-  passport.authenticate("jwt", { session: false }),
-  async (req, res) => {
+    const userOne = req.user
+    if (!userOne.is_admin)  return  res.json({ msg: "admins only"});
+        const id = req.params.id;
+        const club = await Club.find({_id:id})
+        if (!club) return res.json({ message: 'Club does not exist' })
+        const isValidated = validator.updateValidation(req.body)
+        if (isValidated.error) return res.json({ message:  "validations are not satisfied"})
+        const updatedClub = await Club.updateOne(req.body)
+        res.json({ msg: 'Club updated successfully', data: updatedClub })
+    }
+    catch (error) {
+        // We will be handling the error later
+        console.log(error)
+    }
+})
+
+
+
+router.delete('/:id',  passport.authenticate('jwt', {session: false}), async (req, res) => {
     try {
-      const userOne = req.user;
+       // if (!req.session.user_id)  return  res.json({ msg: "not logged in"});
 
-      if (!userOne.is_admin)
-        return res.json({ msg: "Only admins can delete clubs" });
-      const id = req.params.id;
-      const deletedClub = await Club.findByIdAndRemove(id);
-      res.json({ msg: "Club was deleted successfully", data: deletedClub });
-    } catch (error) {
-      // We will be handling the error later
-      console.log(error);
+    const userOne = req.user
+
+    if (!userOne.is_admin)  return  res.json({ msg: "admins only"});
+        const id = req.params.id
+        const deletedClub = await Club.findByIdAndRemove(id)
+        res.json({ msg: 'Club was deleted successfully', data: deletedClub })
     }
-  }
-);
+    catch (error) {
+        // We will be handling the error later
+        console.log(error)
+    }
+})
 
 module.exports = router;
