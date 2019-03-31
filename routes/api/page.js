5bec2a48f4df82f1f4694641d71a2fdff66f1056
ahmademad98 <ahmed.ahmedmahmoud@student.guc.edu.eg>
2019-03-31 20:36:27 +0200

    my_tests

--- routes/api/page.js
+++ routes/api/page.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 const passport = require("passport");
 const Page = require("../../models/Page");
 const User = require("../../models/User").model;
@@ -7,20 +8,20 @@ const Event = require("../../models/Event");
 const validator = require("../../validations/pageValidations");
 const eventValidator = require("../../validations/eventValidations");
 
-//get all pages
+//test done
 router.get("/", async (req, res) => {
   const pages = await Page.find();
   res.json({ data: pages });
 });
 
-//get certain page by id
+//test done
 router.get("/:id", async (request, response) => {
   const id = request.params.id;
   const page = await Page.find({ _id: id });
   response.json({ data: page });
 });
 
-//get events of a certain page by id
+//test done
 router.get("/:id/events", async (req, res) => {
   try {
     var page = await Page.findById(req.params.id);
@@ -33,7 +34,7 @@ router.get("/:id/events", async (req, res) => {
   }
 });
 
-//get members of a certain page
+//test done
 router.get("/:id/members", async (req, res) => {
   try {
     const id = req.params.id;
@@ -45,20 +46,25 @@ router.get("/:id/members", async (req, res) => {
   }
 });
 
-//admins create new page/council
+//done & checked
+// should be for the website admin for the first time to create a council/page/office
+// it posts the whole council
 router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   async (req, res) => {
     try {
+      // if (!req.session.user_id)
+      //   return res.json({ message: "You are not logged in" });
+
       const userOne = req.user;
 
       if (!(userOne.awg_admin === "mun"))
-        return res.json({ mesage: "Only authorized admins can add councils" });
+        return res.json({ msg: "Only mun admins can add councils" });
 
       const isValidated = validator.createValidation(req.body);
       if (isValidated.error)
-        return res.json({ mesage: "validations not satisfied" });
+        return res.json({ msg: "validations not satisfied" });
       const newPage = await Page.create(req.body);
       res.json({ msg: "Page was created successfully", data: newPage });
     } catch (error) {
@@ -67,38 +73,8 @@ router.post(
   }
 );
 
-// admins add events to the page by id
-router.post(
-  "/:id/events",
-  passport.authenticate("jwt", { session: false }),
-  async (req, res) => {
-    try {
-      const userOne = req.user;
-
-      var page = await Page.findById(req.params.id);
-
-      if (
-        !(userOne.mun_role === "secretary_office") &&
-        !(userOne.mun_role === page.role_to_control) &&
-        !(userOne.mun_role === page.name)
-      )
-        return res.json({
-          message: "Only authorized admins can add events to this council"
-        });
-      const isValidated = eventValidator.createValidation(req.body);
-      if (isValidated.error)
-        return res.json({ message: "vaildations not satisfied" });
-      const newEvent = await Event.create(req.body);
-
-      res.json({ data: newEvent });
-    } catch (error) {
-      // We will be handling the error later
-      console.log(error);
-    }
-  }
-);
-
-// admins add members to council by id
+// checked
+// add members to the council if he is logged in and is admin of this page or higher
 router.post(
   "/:id/members",
   passport.authenticate("jwt", { session: false }),
@@ -114,21 +90,26 @@ router.post(
         !(userOne.mun_role === page.name)
       )
         return res.json({
-          message: "Only authorized admins can add members to this entity"
+          msg: "Only MUN admins can add members to this entity"
         });
 
       const guc_id = req.body.guc_id;
-      const userTwo = await User.findOne({ guc_id: guc_id });
-      if (!userTwo)
-        return res.json({ message: "A student with this id does not exists" });
+      const user = await User.findOne({ guc_id: guc_id });
+      if (!user)
+        return res.json({ msg: "A student with this id does not exists" });
 
       const isValidated = validator.addMemberValidation(req.body);
       if (isValidated.error)
-        return res.json({ message: "validations not satisfied" });
+        return res.json({ msg: "validations not satisfied" });
 
       const id = req.params.id;
-      Page.update({ _id: id }, { $push: { members: [req.body] } }).exec();
-      res.json({ msg: "Member was added successfully" });
+      const y = await Page.update(
+        { _id: id },
+        { $push: { members: [req.body] } }
+      ).exec();
+      var page1 = await Page.findById(req.params.id);
+      var x = page1.members[0]._id;
+      res.json({ msg: "Member was added successfully", data: x });
     } catch (error) {
       // We will be handling the error later
       console.log(error);
@@ -136,26 +117,30 @@ router.post(
   }
 );
 
-// admins assign role to member
+// checked
+// assign role members if he is this member the same or page admin
 router.put(
   "/:id/members/set_role",
   passport.authenticate("jwt", { session: false }),
   async (req, res) => {
+    // if (!req.session.user_id)
+    //   return res.json({ message: "You are not logged in" });
+
     const userOne = req.user;
 
     var page = await Page.findById(req.params.id);
 
     const guc_id = req.body.guc_id;
-    const userTwo = await User.findOne({ guc_id: req.body.guc_id });
+    const user = await User.findOne({ guc_id: req.body.guc_id });
 
     if (!(userOne.mun_role === page.name))
-      return res.json({ message: "Only authorized admins can assign roles" });
+      return res.json({ msg: "cannot assign" });
 
-    if (!userTwo)
-      return res.json({ message: "A member with this id does not exists" });
+    if (!user)
+      return res.json({ msg: "A member with this id does not exists" });
 
-    if (!(userTwo.mun_role == "none"))
-      return res.json({ message: "This member is already assigned to a role" });
+    if (!(user.mun_role == "none"))
+      return res.json({ msg: "role already assigned" });
 
     await User.updateOne(
       { guc_id: guc_id },
@@ -163,11 +148,13 @@ router.put(
       { upsert: false }
     );
     const updatedUser = await User.findOne({ guc_id: req.body.guc_id });
-    return res.json({ message: "updated!", user: updatedUser });
+    return res.json({ msg: "updated!", user: updatedUser });
   }
 );
 
-//admins update council by id
+//done & checked
+//this update will be limited for desc. only
+// update the whole council if council admin or higher
 router.put(
   "/:id",
   passport.authenticate("jwt", { session: false }),
@@ -183,14 +170,14 @@ router.put(
         !(userOne.mun_role === page.name)
       )
         return res.json({
-          message: "Only authorized admins can edit this council"
+          msg: "Only admins can update this entity"
         });
 
       const isValidated = validator.updateValidation(req.body);
       if (isValidated.error)
         return res.json({ message: "validations not satisfied" });
       const updatedPage = await Page.updateOne(req.body);
-      res.json({ data: updatedPage });
+      res.json({ msg: "updated", data: updatedPage });
     } catch (error) {
       // We will be handling the error later
       console.log(error);
@@ -198,22 +185,26 @@ router.put(
   }
 );
 
-//admins delete the whole council by id
+//checked
+// delete the whole council
 router.delete(
   "/:id",
   passport.authenticate("jwt", { session: false }),
   async (req, res) => {
     try {
+      // if (!req.session.user_id)
+      //   return res.json({ message: "You are not logged in" });
+
       const userOne = req.user;
 
       if (!(userOne.awg_admin === "mun"))
         return res.json({
-          message: "Only authorized admins can delete this council"
+          msg: "Only admins can delete this entity"
         });
 
       const id = req.params.id;
       const deletedPage = await Page.findByIdAndRemove(id);
-      res.json({ message: "Page was deleted successfully", data: deletedPage });
+      res.json({ msg: "Page was deleted successfully", data: deletedPage });
     } catch (error) {
       // We will be handling the error later
       console.log(error);
@@ -221,12 +212,16 @@ router.delete(
   }
 );
 
-// admins delete certain members from certain council by id
+// checked
+// delete members from the council
 router.delete(
-  "/:id/members/:id1",
+  "/:id/members/:id1/:id2",
   passport.authenticate("jwt", { session: false }),
   async (req, res) => {
     try {
+      // if (!req.session.user_id)
+      //   return res.json({ message: "You are not logged in" });
+
       const userOne = req.user;
 
       var page = await Page.findById(req.params.id);
@@ -237,16 +232,16 @@ router.delete(
         !(userOne.mun_role === page.name)
       )
         return res.json({
-          message: "Only authorized admins can delete members from this council"
+          msg: "Only admins can delete members to this entity"
         });
-
-      const user = await User.findOne({ guc_id: req.body.guc_id });
+      const id2 = req.params.id2;
+      const user = await User.findOne({ guc_id: id2 });
 
       if (!user)
-        return res.json({ message: "A member with this id does not exists" });
+        return res.json({ msg: "A member with this id does not exists" });
 
       await User.updateOne(
-        { guc_id: req.body.guc_id },
+        { guc_id: id2 },
         { mun_role: "none" },
         { upsert: false }
       );
@@ -254,39 +249,7 @@ router.delete(
       const id = req.params.id;
       const id1 = req.params.id1;
       Page.update({ _id: id }, { $pull: { members: { _id: id1 } } }).exec();
-      res.json({ msg: "Member was deleted successfully" });
-    } catch (error) {
-      // We will be handling the error later
-      console.log(error);
-    }
-  }
-);
-
-// admins delete certain event from certain council by id
-router.delete(
-  "/:id/events/:id1",
-  passport.authenticate("jwt", { session: false }),
-  async (req, res) => {
-    try {
-      const userOne = req.user;
-
-      var page = await Page.findById(req.params.id);
-
-      if (
-        !(userOne.mun_role === "secretary_office") &&
-        !(userOne.mun_role === page.role_to_control) &&
-        !(userOne.mun_role === page.name)
-      )
-        return res.json({
-          message: "Only authorized admins can delete events from this council"
-        });
-
-      const id1 = req.params.id1;
-      const deletedEvent = await Event.findByIdAndRemove(id1);
-      res.json({
-        message: "Event was deleted successfully",
-        data: deletedEvent
-      });
+      res.json({ msg: "Members was deleted successfully" });
     } catch (error) {
       // We will be handling the error later
       console.log(error);
