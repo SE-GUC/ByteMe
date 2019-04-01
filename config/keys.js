e956bfa820057f131bee42ec785890f9a90beeab
Omar Hussein <omarhatem101@gmail.com>
2019-04-01 12:39:27 +0200

    replaced config files

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,5 @@
-if (process.env.NODE_ENV === 'production')
-    module.exports = require('./keys_prod')
-else
-    module.exports = require('./keys_dev')
-
-    module.exports = {
-        mongoURI: 'mongodb://ahmaademaad:1234@munbytemecluster-shard-00-00-da3gg.mongodb.net:27017,munbytemecluster-shard-00-01-da3gg.mongodb.net:27017,munbytemecluster-shard-00-02-da3gg.mongodb.net:27017/test?ssl=true&replicaSet=MUNByteMeCluster-shard-0&authSource=admin&retryWrites=true',
-    }
-    
+module.exports = {
+  secretOrKey: "verysecretkey",
+  mongoURI:
+    "mongodb://ahmaademaad:1234@munbytemecluster-shard-00-00-da3gg.mongodb.net:27017,munbytemecluster-shard-00-01-da3gg.mongodb.net:27017,munbytemecluster-shard-00-02-da3gg.mongodb.net:27017/test?ssl=true&replicaSet=MUNByteMeCluster-shard-0&authSource=admin&retryWrites=true"
+};
