22f2ea256a077e9fc6b936f160101e7ba625d127
kareemserry <kareem-serry@hotmail.com>
2019-04-04 00:07:18 +0200

    updated navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { Navbar, NavDropdown, Nav } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
 import "./App.css";
 import logo from "./logo.svg";
+import Header from "./components/HeaderNavbar";
 
 class App extends Component {
   render() {
@@ -13,35 +14,7 @@ class App extends Component {
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
           crossorigin="anonymous"
         />
-        <Navbar bg="blue" variant="dark" sticky="top">
-          <Navbar.Brand href="#home">GUCMUN</Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#announcements">Announcements</Nav.Link>
-            <NavDropdown title="Councils" id="basic-nav-dropdown">
-              {/* neb2a n7ot for loop t-generate council dropdown items */}
-              <NavDropdown.Item href="#action/3.1">
-                Council hena
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.2">
-                Council tany
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.3">
-                Council talet
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#events">Events</Nav.Link>
-            <Nav.Link href="#library">Library</Nav.Link>
-            <Nav.Link href="#merchandise">Merchandise</Nav.Link>
-            <Nav.Link href="#faq">FAQ</Nav.Link>
-          </Nav>
-          {/* neb2a n7ot if t-check law logged in */}
-          <Navbar.Text>
-            Signed in as: <a href="#login">Omar Hussein</a>
-          </Navbar.Text>
-        </Navbar>
+        <Header />
         <Navbar bg="black" fixed="bottom">
           <Navbar.Brand href="#home" className="mr-auto">
             <img
