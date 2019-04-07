5344097b65e04bca41f5409ff59b6e0ffbb3a264
kareemserry <kareem-serry@hotmail.com>
2019-04-07 23:54:47 +0200

    merge conflict update

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -5,87 +5,87 @@ import "./User.css";
 import "../App.css";
 
 class User extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      user: this.props.user
-    };
-  }
+        this.state = {
+            user: this.props.user
+        };
+    }
 
-  render() {
-    const {
-      email,
-      first_name,
-      last_name,
-      birth_date,
-      guc_id,
-      mun_role,
-      picture_ref
-    } = this.state.user;
+    render() {
+        const {
+            email,
+            first_name,
+            last_name,
+            birth_date,
+            guc_id,
+            mun_role,
+            picture_ref
+        } = this.state.user;
 
-    return (
-      <Container className="user">
-        <Row>
-          <Media>
-            <img
-              width={128}
-              height={128}
-              className="mr-3"
-              src={
-                picture_ref
-                  ? picture_ref
-                  : "https://www.watsonmartin.com/wp-content/uploads/2016/03/default-profile-picture.jpg"
-              }
-              alt="Generic placeholder"
-            />
-            <Media.Body>
-              <h5>
-                {(first_name + " " + last_name)
-                  .split(" ")
-                  .map(i => i[0].toUpperCase() + i.substring(1).toLowerCase())
-                  .join(" ")}
-              </h5>
-            </Media.Body>
-          </Media>
-        </Row>
-        <Row>
-          <Col className="header" xs={2}>
-            EMAIL:
+        return (
+            <Container className="user">
+                <Row className="user-row">
+                    <Media>
+                        <img
+                            width={128}
+                            height={128}
+                            className="mr-3"
+                            src={
+                                picture_ref
+                                    ? picture_ref
+                                    : "https://www.watsonmartin.com/wp-content/uploads/2016/03/default-profile-picture.jpg"
+                            }
+                            alt="Generic placeholder"
+                        />
+                        <Media.Body>
+                            <h5>
+                                {(first_name + " " + last_name)
+                                    .split(" ")
+                                    .map(i => i[0].toUpperCase() + i.substring(1).toLowerCase())
+                                    .join(" ")}
+                            </h5>
+                        </Media.Body>
+                    </Media>
+                </Row>
+                <Row className="user-row">
+                    <Col className="user-header" xs={2}>
+                        EMAIL:
           </Col>
-          <Col className="other">{email}</Col>
-        </Row>
-        <Row>
-          <Col className="header" xs={2}>
-            BIRTH DATE:
+                    <Col className="user-other">{email}</Col>
+                </Row>
+                <Row className="user-row">
+                    <Col className="user-header" xs={2}>
+                        BIRTH DATE:
           </Col>
-          <Col className="other">{birth_date.toString().split("T")[0]}</Col>
-        </Row>
-        <Row>
-          <Col className="header" xs={2}>
-            GUC ID:
+                    <Col className="user-other">{birth_date.toString().split("T")[0]}</Col>
+                </Row>
+                <Row className="user-row">
+                    <Col className="user-header" xs={2}>
+                        GUC ID:
           </Col>
-          <Col className="other">{guc_id}</Col>
-        </Row>
-        <Row>
-          <Col className="header" xs={2}>
-            EMAIL:
+                    <Col className="user-other">{guc_id}</Col>
+                </Row>
+                <Row className="user-row">
+                    <Col className="user-header" xs={2}>
+                        EMAIL:
           </Col>
-          <Col className="other">{email}</Col>
-        </Row>
-        {mun_role !== "none" ? (
-          <Row>
-            <Col className="header" xs={2}>
-              ROLE:
+                    <Col className="user-other">{email}</Col>
+                </Row>
+                {mun_role !== "none" ? (
+                    <Row className="user-row">
+                        <Col className="user-header" xs={2}>
+                            ROLE:
             </Col>
-            <Col className="other">{email}</Col>
-          </Row>
-        ) : (
-          <></>
-        )}
-      </Container>
-    );
-  }
+                        <Col className="user-other">{email}</Col>
+                    </Row>
+                ) : (
+                        <></>
+                    )}
+            </Container>
+        );
+    }
 }
 
 export default User;
