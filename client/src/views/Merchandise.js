d41c86eb47a2cca2cf42477663cfdfeace86d5d1
Omar Hussein <omarhatem101@gmail.com>
2019-04-10 19:45:12 +0200

    creation UI

--- client/src/views/Merchandise.js
+++ client/src/views/Merchandise.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Product from "../components/Product";
 import "./Merchandise.css";
 import API from "../utils/API";
+import { Button, Modal, InputGroup, FormControl } from "react-bootstrap";
+import iconAdd from "../icons/plus.svg";
 
 // First we create our class
 class Merchandise extends Component {
@@ -9,12 +11,15 @@ class Merchandise extends Component {
   constructor(props) {
     super(props);
     this.updateProducts = this.updateProducts.bind(this);
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
     // Next we establish our state
     this.state = {
       isLoading: true,
       products: [],
       user: props.user,
-      isEditing: false
+      isEditing: false,
+      showAddProductWindow: false
     };
   }
   // The render function, where we actually tell the browser what it should show
@@ -24,6 +29,13 @@ class Merchandise extends Component {
         <h1 style={{ margin: "15px" }}>GUCMUN SWAG</h1>
         <div className="merchandise">
           <div className="product-group">
+            <Button
+              variant="warning"
+              className="product-create-button"
+              onClick={this.handleShow}
+            >
+              <img src={iconAdd} />
+            </Button>
             {this.state.products.map(product => (
               <Product
                 id={product._id}
@@ -36,6 +48,52 @@ class Merchandise extends Component {
             ))}
           </div>
         </div>
+        <Modal
+          show={this.state.showAddProductWindow}
+          onHide={this.handleClose}
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Add a new product</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            {/* name */}
+            <InputGroup className="mb-3">
+              <FormControl
+                placeholder="Product Name"
+                aria-label="Product Name"
+              />
+            </InputGroup>
+            {/* price */}
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend>
+                <InputGroup.Text>EGP</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                placeholder="Product Price"
+                aria-label="Product Price"
+              />
+              <InputGroup.Append>
+                <InputGroup.Text>.00</InputGroup.Text>
+              </InputGroup.Append>
+            </InputGroup>
+            {/* description */}
+            <InputGroup className="mb-3">
+              <FormControl
+                placeholder="Product description"
+                aria-label="Product description"
+              />
+            </InputGroup>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+            </Button>
+            <Button variant="danger" onClick={this.handleClose}>
+              I know what i'm doing
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     );
   }
@@ -50,6 +108,12 @@ class Merchandise extends Component {
     } catch (e) {
       console.log(`😱 Axios request failed: ${e}`);
     }
+  }
+  handleClose() {
+    this.setState({ showAddProductWindow: false });
+  }
+  handleShow() {
+    this.setState({ showAddProductWindow: true });
   }
 }
 
